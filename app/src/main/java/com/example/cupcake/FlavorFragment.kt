7d5cc3b21857312b4d7faa345a1580f819cec768
Dmytro Siasko <dima.siasko@gmail.com>
2023-11-04 19:59:50 +0200

    init app created

--- app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.cupcake
 
 import android.os.Bundle
@@ -23,14 +9,8 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.cupcake.databinding.FragmentFlavorBinding
 
-/**
- * [FlavorFragment] allows a user to choose a cupcake flavor for the order.
- */
 class FlavorFragment : Fragment() {
 
-    // Binding object instance corresponding to the fragment_flavor.xml layout
-    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
-    // when the view hierarchy is attached to the fragment.
     private var binding: FragmentFlavorBinding? = null
 
     override fun onCreateView(
@@ -50,17 +30,10 @@ class FlavorFragment : Fragment() {
         }
     }
 
-    /**
-     * Navigate to the next screen to choose pickup date.
-     */
     fun goToNextScreen() {
         Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
     }
 
-    /**
-     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
-     * is being removed. As a result, clear out the binding object.
-     */
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.cupcake
 
 import android.os.Bundle
@@ -23,14 +8,9 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.cupcake.databinding.FragmentStartBinding
 
-/**
- * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
- */
+
 class StartFragment : Fragment() {
 
-    // Binding object instance corresponding to the fragment_start.xml layout
-    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
-    // when the view hierarchy is attached to the fragment.
     private var binding: FragmentStartBinding? = null
 
     override fun onCreateView(
@@ -53,17 +33,10 @@ class StartFragment : Fragment() {
         }
     }
 
-    /**
-     * Start an order with the desired quantity of cupcakes and navigate to the next screen.
-     */
     fun orderCupcake(quantity: Int) {
         Toast.makeText(activity, "Ordered $quantity cupcake(s)", Toast.LENGTH_SHORT).show()
     }
 
-    /**
-     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
-     * is being removed. As a result, clear out the binding object.
-     */
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
